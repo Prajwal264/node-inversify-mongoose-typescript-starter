47d9bbad2c436fb0c1dfa4b634821fe86ad46eda
Prajwal264 <prajwalp264@gmail.com>
2023-06-16 23:02:19 +0530

    feat: changes in index.ts source

--- source/src/index.ts
+++ source/src/index.ts
@@ -1,4 +1,14 @@
+import 'reflect-metadata';
 import express, { Application } from 'express';
+import cors from 'cors';
+import { InversifyExpressServer } from 'inversify-express-utils';
+import mongoose from 'mongoose';
+import container from './inversify.cofig';
+import './controllers/index.controller';
+
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 /**
  *
@@ -63,6 +73,7 @@ class Server {
    */
   private async setup() {
     this.createExpressApplication();
+    await this.connectToDb();
   }
 
   /**
@@ -72,7 +83,19 @@ class Server {
    * @memberof Server
    */
   private createExpressApplication() {
-    this.app = express();
+    const app = express();
+    app.use(express.json());
+    app.use(cors({
+      credentials: true,
+      origin: process.env.CLIENT_ORIGIN,
+    }));
+    const server = new InversifyExpressServer(container, null, { rootPath: '/api' }, app);
+    this.app = server.build();
+  }
+
+  private async connectToDb() {
+    const MONGO_URI = 'mongodb://127.0.0.1:27017/app?compressors=zlib';
+    await mongoose.connect(MONGO_URI);
   }
 
   /**
@@ -83,9 +106,7 @@ class Server {
    */
   private listen() {
     const { port } = this;
-    this.app.listen(port, () => {
-      console.log(`Server is running at ${port}`);
-    });
+    this.app.listen(port, () => `Server running ${port}`);
   }
 }
 
